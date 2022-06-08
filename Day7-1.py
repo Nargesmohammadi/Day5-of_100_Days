a061e26cc72ab3c2f5aef69f88247c1ab51c851f
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-06-08 13:55:44 +0430

    I

--- Day7-1.py
+++ Day7-1.py
@@ -1,9 +1,56 @@
 import random
 
+HANGMAN_PICS = ['''
+   +---+
+       |
+       |
+       |
+  ===''', '''
+   +---+
+   O   |
+       |
+       |
+  ===''', '''
+   +---+
+   O   |
+   |   |
+       |
+  ===''', '''
+   +---+
+   O   |
+  /|   |
+       |
+  ===''', '''
+   +---+
+   O   |
+  /|\  |
+       |
+  ===''', '''
+   +---+
+   O   |
+  /|\  |
+  /    |
+  ===
+''', '''
+   +---+
+   O   |
+  /|\  |
+  / \  |
+  ===
+  ''', '''
+   +---+
+   |   |
+   O   |
+  /|\  |
+  / \  |
+  ===''']
+
 # step1
 word_list = ["evolution", "canada", "farmer", "advance"]
 the_word = random.choice(word_list)
 the_blank = len(the_word)
+
+lives = []
 
 # testing code
 print(f"Past, the solution is {the_word}")
@@ -23,8 +70,17 @@ while not end_of_game:
         letter = the_word[position]
         if letter == the_guess:
             display[position] = letter
+
+if the_guess not in the_word:
+    lives -= 1
+    if letter == 0:
+        end_of_game = True
+        print("You loss")
 print(display)
 
 if "_" not in display:
     end_of_game = True
     print("You win")
+
+
+print(HANGMAN_PICS[lives])
