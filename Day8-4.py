8ef04c7997b4ddd805d59a16c5d0c2ad780d57c4
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-06-10 22:05:13 +0430

    combine to function

--- Day8-4.py
+++ Day8-4.py
@@ -8,26 +8,39 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(my_text, shift_amount):
-    cipher_text = ""
-    for letter in my_text:
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "encode":
+        shift_amount *= - 1
+    for letter in start_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+    print(f"The encoded text is {end_text}")
 
 
-def decrypt(cipher_text, shift_amount):
-    my_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        my_text += alphabet[new_position]
-    print(f"The decoded text is {my_text}")
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+# def encrypt(my_text, shift_amount):
+# cipher_text = ""
+# for letter in my_text:
+# position = alphabet.index(letter)
+# new_position = position + shift_amount
+# new_letter = alphabet[new_position]
+# cipher_text += new_letter
+# print(f"The encoded text is {cipher_text}")
 
 
-if direction == "encode":
-    encrypt(my_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
+# def decrypt(cipher_text, shift_amount):
+# my_text = ""
+# for letter in cipher_text:
+# position = alphabet.index(letter)
+# new_position = position - shift_amount
+# my_text += alphabet[new_position]
+# print(f"The decoded text is {my_text}")
+
+
+# if direction == "encode":
+# encrypt(my_text=text, shift_amount=shift)
+# elif direction == "decode":
+# decrypt(cipher_text=text, shift_amount=shift)
