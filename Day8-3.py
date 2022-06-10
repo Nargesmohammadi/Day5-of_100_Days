d30d661f47b363fda0089edf204261f951469691
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-06-10 21:32:43 +0430

    .

--- Day8-3.py
+++ Day8-3.py
@@ -4,7 +4,7 @@ import math
 def prime_checker(number):
     is_prime = True
     for i in range(2, number):
-        if number % i == 0:
+        if math.sqrt(number % i == 0):
             is_prime = False
     if is_prime:
         print("It's a prime number.")
