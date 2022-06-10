02a60bc331903a60689c84f85632dcf7d37c57b0
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-06-10 10:05:33 +0430

    I create a function with more than one input and a function with keyword arguments.

--- Day8-1.py
+++ Day8-1.py
@@ -1,8 +1,7 @@
-# Rewiew:
-# Creat a function called greet().
+# # # Create a function called greet().
 
 
-# simple function
+# # simple function
 
 # def greet():
 # print("Hello Mitra")
@@ -13,16 +12,31 @@
 # print(greet())
 
 
-# function that allows for input
+# # function that allows for input
 
-def greet_with_name(name):
+# def greet_with_name(name):
+# print(f"Hello {name}")
+# print(f"How do you do {name}?")
+# print("Isn't the weather nice today?")
+
+
+# greet_with_name("Mitra")
+
+# # function with more than one input.
+
+def greet_with(name, location):
     print(f"Hello {name}")
-    print(f"How do you do {name}?")
-    print("Isn't the weather nice today?")
+    print(f"what is it like in {location}")
 
 
-greet_with_name("Mitra")
+# greet_with("Mitra", "Shiraz")
+# greet_with("Shiraz", "Mitra")
+
+# # functions with keyword arguments
+
+greet_with(name="Mitra", location="Shiraz")
+greet_with(location="Shiraz", name="Mitra")
 
 
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
+# # # Write 3 print statements inside the function.
+# # # Call the greet() function and run your code.
