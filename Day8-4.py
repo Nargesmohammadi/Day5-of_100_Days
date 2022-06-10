c41f7c9e92c949d7093558f20dff2fff0ab6243c
nNargesmohammadi <NargesMohammadii@protonmail.com>
2022-06-10 23:20:23 +0430

    create one function instead two and use the while loop.

--- Day8-4.py
+++ Day8-4.py
@@ -3,44 +3,31 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
 
 
-def caesar(start_text, shift_amount, cipher_direction):
-    end_text = ""
-    if cipher_direction == "encode":
-        shift_amount *= - 1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
-    print(f"The encoded text is {end_text}")
+    def caesar(start_text, shift_amount, cipher_direction):
+        end_text = ""
+        if cipher_direction == "encode":
+            shift_amount *= - 1
+        for char in start_text:
+            if char in alphabet:
+                position = alphabet.index(char)
+                new_position = position + shift_amount
+                end_text += alphabet[new_position]
+            else:
+                end_text += char
+        print(f"Here's the {cipher_direction}d result: {end_text}")
 
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-# def encrypt(my_text, shift_amount):
-# cipher_text = ""
-# for letter in my_text:
-# position = alphabet.index(letter)
-# new_position = position + shift_amount
-# new_letter = alphabet[new_position]
-# cipher_text += new_letter
-# print(f"The encoded text is {cipher_text}")
-
-
-# def decrypt(cipher_text, shift_amount):
-# my_text = ""
-# for letter in cipher_text:
-# position = alphabet.index(letter)
-# new_position = position - shift_amount
-# my_text += alphabet[new_position]
-# print(f"The decoded text is {my_text}")
-
-
-# if direction == "encode":
-# encrypt(my_text=text, shift_amount=shift)
-# elif direction == "decode":
-# decrypt(cipher_text=text, shift_amount=shift)
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("goodbye.")
